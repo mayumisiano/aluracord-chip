db7485086bb5d2c4efd823aa36818b980f0ac2f0
Paula Mayumi <mayumi_siano10@hotmail.com>
2022-01-27 22:51:39 -0300

    4o dia da Imersao React

--- pages/chat.js
+++ pages/chat.js
@@ -1,11 +1,28 @@
 import { Box, Text, TextField, Image, Button } from '@skynexui/components';
 import React from 'react';
 import appConfig from '../config.json';
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzMyOTMwNCwiZXhwIjoxOTU4OTA1MzA0fQ.hsoTvBHSbsWFs1slqG5tHB8dT0qJ2XPWnStFAwyo648';
+const SUPABASE_URL = 'https://sscvjyreuwxjkmdzudtl.supabase.co';
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default function ChatPage() {
     const [mensagem, setMensagem] = React.useState('');
     const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
 
+    React.useEffect(() => {
+        supabaseClient
+            .from('mensagens')
+            .select('*')
+            .order('id', { ascending: false})
+            .then(({ data }) => {
+                console.log('Dados da consulta:', data)
+                setListaDeMensagens(data);
+            });    
+    }, []);
+
+    
     /*
     // Usuário
     - Usuário digita no campo textarea
@@ -19,16 +36,24 @@ export default function ChatPage() {
     */
     function handleNovaMensagem(novaMensagem) {
         const mensagem = {
-            id: listaDeMensagens.length + 1,
+            // id: listaDeMensagens.length + 1,
             de: 'mayumisiano',
             texto: novaMensagem,
         };
 
-        setListaDeMensagens([
-            mensagem,
-            ...listaDeMensagens,
-        ]);
-        setMensagem('');
+        supabaseClient
+            .from('mensagens')
+            .insert([
+                mensagem
+            ])
+            .then(({ data }) => {
+                console.log('Criando mensagem:', data)
+                setListaDeMensagens([
+                   data[0],
+                    ...listaDeMensagens,
+                ]);                
+            });
+        setMensagem('');        
     }
 
     return (
